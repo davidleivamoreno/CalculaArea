f62b304da6d74732461d3c9787c50c688b93e746
Davidlaptop <leivamorenodavid@gmail.com>
2023-11-16 09:48:00 +0100

    Fusion de proyectos con merge sin conflicto

--- Main.java
+++ Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
        Rectángulo rectangulo=new Rectángulo(5.1 , 2.8);
       double arear= rectangulo.calculaArea();
+      Circulo circulo=new Circulo(6.4);
+      double areacir= circulo.calculaAreaCirculo();
+      Pentagono pentagono=new Pentagono(5,7.5);
+      double areapent=pentagono.calculaAreaPentagono;
+      hexagono hexagono=new hexagono(5.5,3.1);
     }
 }
