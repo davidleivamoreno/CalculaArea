65e69a49a5ec7b68d331d116e89a0c20be46613a
Davidlaptop <leivamorenodavid@gmail.com>
2023-11-16 10:02:30 +0100

    Ultimos arreglos de calculo de area

--- Main.java
+++ Main.java
@@ -9,5 +9,7 @@ public class Main {
       pentagono pentagono=new pentagono(5,7.5);
       double areapent=pentagono.calculaAreaPentagono();
       hexagono hexagono=new hexagono(5.5,3.1);
+      double areahex=hexagono.calculaAreaHexagono();
+      System.out.println("Todos areas: \n Area rectangulo:"+arear+"\n Area Circulo: "+areacir+"\n Area Pentagono: "+areapent+"\n Area hexagono: "+areahex);
     }
 }
